893579a58c1264d3db47ebf9a62f9b8446d17af2
LEE JUN HEE <ljhg1124@gmail.com>
2020-03-15 20:02:53 +0900

    time update
    시간 기능 추가.

---
+++ clock.js
@@ -0,0 +1,21 @@
+
+const clockContainer = document.querySelector(".js_clock");
+const clock = clockContainer.querySelector("span");
+
+function createZero(val)
+{
+    return val < 10 ? `0${val}`: val;
+}
+
+(function init(){
+
+    setInterval((function getTime(){
+        const date = new Date();
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const seconds = date.getSeconds();
+
+        clock.innerText = `${createZero(hours)}:${createZero(minutes)}:${createZero(seconds)}`
+    }), 1000);
+})()
+
